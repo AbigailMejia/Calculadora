4d8ff17c06d07eb8efb81bd64911e5e5439c775c
Marcia Abigáil Mejía Reyes <201900027@colsanjose.edu.gt>
2020-04-17 19:02:40 -0600

    :)

--- Calcular.php
+++ Calcular.php
@@ -6,28 +6,44 @@ $comparar=$_POST['operacion'];
 if ($comparar==1)
 {
  $total=$valor1+$valor2;
-
-echo "el resultado es: ".$total;
 }
 
 else if ($comparar==2)
 {
  $total=$valor1-$valor2;
 
-echo "el resultado es: ".$total;
 }
 
 else if ($comparar==3)
 {
  $total=$valor1*$valor2;
 
-echo "el resultado es: ".$total;
 }
 
 else if ($comparar==4)
 {
  $total=$valor1/$valor2;
 
-echo "el resultado es: ".$total;
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text.css" href="estilos.css"/>
+</head>
+
+<body>
+<header>
+<div align="center" id="Resultado">
+<h1>resultado</h1>
+</div>
+</header>
+
+<section>
+<div align="center" id="mostrar">
+<p><?php echo $total; ?> </p>
+</div>
+</section>
+</body>
+</html>
